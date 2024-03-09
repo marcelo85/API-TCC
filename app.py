6f98aefd5b383eb2f8380daae777404ab5c83ee1
Marcelo Campos <mc1985@terra.com.br>
2024-03-09 11:03:27 -0300

    Criar a App do TCC.

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+app.config['TITLE'] = 'Meu site'
+
 
 @app.route('/')
 def index():
     animais = ['Coiote', 'Lince Pardo', 'Veado da Virgínia']
-    return animais[0]
+    return render_template('index.html', animais=animais)
