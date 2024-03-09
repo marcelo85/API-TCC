dcb7cdce32a31038b13327e0b48b9508434c56d6
Marcelo Campos <mc1985@terra.com.br>
2024-03-09 11:45:39 -0300

    Criar a App do TCC.

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
-from flask import Flask, render_template
+from dynaconf import FlaskDynaconf
+from flask import Flask, abort, render_template
+from flask_admin import Admin
+from flask_admin.base import AdminIndexView
+from flask_admin.contrib import sqla
+from flask_simplelogin import SimpleLogin, login_required
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['TITLE'] = 'Meu site'
+FlaskDynaconf(app)
+db = SQLAlchemy(app)
 
 
-@app.route('/')
+def verifica_login(usuario):
+    return usuario.get("username") == 'admin' and usuario.get("password") == '123'
+
+SimpleLogin(app, login_checker=verifica_login
+
+# Proteger o admin com login via Monkey Patch
+AdminIndexView._handle_view = login_required(AdminIndexView._handle_view)
+sqla.ModelView._handle_view = login_required(sqla.ModelView._handle_view)
+admin = Admin(app, name=app.config.TITLE, template_mode="bootstrap3")
+
+@app.cli.command()
+def create_db():
+    """Creates sqlite database"""
+    db.create_all()
+
+class Animais(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome_cientifico = db.Column(db.String(255))
+    nome_popular = db.Column(db.String(255))
+    numero = db.Column(db.Numeric())
+
+admin.add_view(sqla.ModelView(Animais, db.session))
+
+@app.route('/'))
 def index():
-    animais = ['Coiote', 'Lince Pardo', 'Veado da Virgínia']
+    animais = Animais.query.all()
     return render_template('index.html', animais=animais)
+
+@app.route('/animal/<animal_id>')
+def animal(animal_id):
+    animal = Animais.query.filter_by(id=animal_id).first() or abort(404, "Animal não encontrado")
+    return render_template('animal.html', animal=animal)
